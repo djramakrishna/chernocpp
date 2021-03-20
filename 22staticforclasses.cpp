f2ef59ba0251a925162c804b22e66876a280a316
D J <jnanaramakrishnad@gmail.com>
2021-03-21 01:42:15 +0530

    Update 22staticforclasses.cpp

--- 22staticforclasses.cpp
+++ 22staticforclasses.cpp
@@ -1,5 +1,5 @@
 // 2 - static variable inside a class or struct - this variable is going to share memory with all the instances of the class or struct 
-// i.e, in all of the instances of the class or struct there is going to be only one instance of that variable, means only one 
+// i.e, in all of the instances of the class or struct there is going to be only one instance of that variable, means only one  
 // If used with a variable, if one of the intances of the class changes this static variable, it's going to reflect that changes across 
 // all the instances, as there is only one variable though there are many instances of the class.
 
@@ -13,7 +13,24 @@ struct Entity
     {
         std::cout << x << "," << y << std::endl;
     }
+    
+    //Every non static methods that is inside a class always gets an instance of the current class as a parameter 
+    //Static methods cannot access non static variables 
+    //Static method does not have a class instance  
+    //Static method is same as if you wrote a method outside of a class 
+    
+    static void Print2()
+    {
+        std::cout << x << " In static function " << y << std::endl;
+    }
+    
 };
+
+//Below is actually how a nonstatic method in a class looks likes
+static void Print(Entity e)
+{
+    std::cout << e.x << "," << e.y << std::endl;
+}
 
 //define those static variables else we might receive an error in main for those static variables
 
@@ -36,7 +53,11 @@ int main()
     //e1.y = 8;
     e.Print();
     e1.Print();
+    
     Entity::Print();
     Entity::Print();
+    
+    Entity::Print2();
+    Entity::Print2();
     
 }
